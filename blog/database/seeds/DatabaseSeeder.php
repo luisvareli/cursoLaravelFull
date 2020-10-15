a5b1224dc2ee02ca33eb1ba1bc028483c8d46817
Luis Vargas <lewiis29@gmail.com>
2020-10-15 16:18:19 -0600

    Creación de tablas, entidades y datos de prueba

--- blog/database/seeds/DatabaseSeeder.php
+++ blog/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'name'=>'Luis Vargas E',
+            'email'=>'Luis@admin.com',
+            'password'=>bcrypt('123456'),
+        ]);
+        factory(App\Post::class, 24)->create();
     }
 }
