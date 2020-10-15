0804610b8facb335f7b37b5344d429183ecf48d3
Luis Vargas <lewiis29@gmail.com>
2020-10-15 14:27:22 -0600

    Relaciones de tablas

--- eloquent/routes/web.php
+++ eloquent/routes/web.php
@@ -22,3 +22,26 @@ Route::get('eloquent', function () {
         echo "$post->id $post->title <br>";
     }
 });
+
+Route::get('posts', function () {
+    $posts = Post::get();
+
+    foreach ($posts as $post){
+        echo "
+        $post->id
+        <strong>{$post->user->name}</strong>
+        $post->title <br>";
+    }
+});
+
+use App\User;
+Route::get('users', function () {
+    $users= User::all();
+
+    foreach ($users as $user){
+        echo "
+        $user->id
+        <strong>{$user->name}</strong>
+        {$user->posts->count()} posts<br>";
+    }
+});
