1dee66f9f31db46a1556f20c2811a914a4eccb53
Luis Vargas <lewiis29@gmail.com>
2020-10-16 16:19:15 -0600

    Show con TDD

--- api-new/app/Http/Controllers/Api/PostController.php
+++ api-new/app/Http/Controllers/Api/PostController.php
@@ -44,7 +44,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return response()->json($post);
     }
 
     /**
--- api-new/tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ api-new/tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers\Api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Post;
 
 class PostControllerTest extends TestCase
 {
@@ -32,5 +33,22 @@ class PostControllerTest extends TestCase
 
         $response->assertStatus(422)
             ->assertJsonValidationErrors('title');
+    }
+    public function test_show()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->json('GET',"/api/posts/$post->id");
+
+        $response->assertJsonStructure(['id','title','created_at','updated_at'])
+            ->assertJson(['title' =>$post->title])
+            ->assertStatus(200);
+    }
+    public function test_404_show()
+    {
+
+        $response = $this->json('GET','/api/posts/1000');
+
+        $response->assertStatus(404);
     }
 }
